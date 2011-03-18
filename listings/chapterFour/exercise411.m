b8bbceb15074bb9d23df463efb358bbe39293676
Massimo Nocentini <massimo.nocentini@gmail.com>
2011-03-18 15:21:36 +0000

    fixed exercise 4.11
    git-svn-id: https://mnserver.homeunix.net/svn/MyRepository/Study/LaureaInformatica/SecondoAnno/Calcolo Numerico/Elaborato@710 a5b5ea6b-43ee-a641-ac96-51f52e225603

--- listings/chapterFour/exercise411.m
+++ listings/chapterFour/exercise411.m
@@ -1,65 +1,91 @@
 function [] = exercise411()
-    toInterpolateAscisseVector = linspace(-1, 1, 30)';
 
-    interpolationOrdinateVector = toInterpolateAscisseVector;
-    for i=1:length(toInterpolateAscisseVector)
-        interpolationOrdinateVector(i) = realFunction(toInterpolateAscisseVector(i));
+    # build the vectors for valuate the interpolation and study the error.
+    evaluateSetDimension = 100;
+    rungeEvaluationInterval = linspace(-5, 5, evaluateSetDimension)';
+    bernsteinEvaluationInterval = linspace(-1, 1, evaluateSetDimension)';
+
+    # have the same dimension
+    rungeEvaluatedFunctionValuesVector = rungeEvaluationInterval;
+    bernsteinEvaluatedFunctionValuesVector = bernsteinEvaluationInterval;
+    
+     # build the real functions values for evaluate real functions
+    for i=1:evaluateSetDimension
+        rungeEvaluatedFunctionValuesVector(i) = rungeRealFunction(...
+            rungeEvaluationInterval(i));
+
+        bernsteinEvaluatedFunctionValuesVector(i) = bernsteinRealFunction(...
+            bernsteinEvaluationInterval(i));
+    end 
+
+    # build the error vector
+    errorVectorDimension = 100;
+    rungeErrorVector = ones(errorVectorDimension,1);
+    bernsteinErrorVector = ones(errorVectorDimension,1);
+
+    # start from 1 to have at least one ascisse for Bernstein interval domain.
+    for n=1:errorVectorDimension
+
+        # build the ascisse where I want to interpolate the functions
+        # the limits {-5, 5, -1, 1} are fixed by definition of the exercise
+        # fix the length of the set of interpolation ascisse
+        #ascisseDimension = 10;
+        rungeInterpolationAscisseVector = linspace(-5, 5, n)';
+        bernsteinInterpolationAscisseVector = linspace(-1, 1, n)';
+
+        # setup the arrays of function values with the correct dimensions
+        rungeInterpolationOrdinateVector = rungeInterpolationAscisseVector;
+        bernsteinInterpolationOrdinateVector = bernsteinInterpolationAscisseVector;
+
+        # build the real functions values for building the interpolation polynomion
+        for i=1:n
+            rungeInterpolationOrdinateVector(i) = rungeRealFunction(...
+                rungeInterpolationAscisseVector(i));
+
+            bernsteinInterpolationOrdinateVector(i) = bernsteinRealFunction(...
+                bernsteinInterpolationAscisseVector(i));
+        end    
+
+        # compute the differenze divise vectors for the two functions.
+        rungeDifferenzeDiviseVector = differenzeDiviseEngine(rungeInterpolationAscisseVector, ...
+            rungeInterpolationOrdinateVector);
+
+        bernsteinDifferenzeDiviseVector = differenzeDiviseEngine(bernsteinInterpolationAscisseVector, ...
+            bernsteinInterpolationOrdinateVector);
+
+        # compute the polynomion and valuate on an interval
+        rungeInterpolatedOrdinateVector = HornerGeneralizzato(rungeEvaluationInterval, ...
+            rungeInterpolationAscisseVector, rungeDifferenzeDiviseVector);
+
+        bernsteinInterpolatedOrdinateVector = HornerGeneralizzato(bernsteinEvaluationInterval, ...
+            bernsteinInterpolationAscisseVector, bernsteinDifferenzeDiviseVector);
+
+        # calculate the error
+        rungeErrorVector(n) = max(abs(rungeEvaluatedFunctionValuesVector ...
+             - rungeInterpolatedOrdinateVector));
+        bernsteinErrorVector(n) = max(abs(bernsteinEvaluatedFunctionValuesVector ...
+             - bernsteinInterpolatedOrdinateVector));
+
     end
 
-    # linspace return a row vector, so I transpose in order to obtain a column 
-    # vector
-    interpolationAscisseVector1 = linspace(-1, 1, 15)';
-    interpolatedFunctionValuesVector1 = exercise_Internal(...
-        interpolationAscisseVector1, toInterpolateAscisseVector);
-
-    # linspace return a row vector, so I transpose in order to obtain a column 
-    # vector
-    interpolationAscisseVector2 = linspace(-1, 1, 10)';
-    interpolatedFunctionValuesVector2 = exercise_Internal(...
-        interpolationAscisseVector2, toInterpolateAscisseVector);
-
-    # linspace return a row vector, so I transpose in order to obtain a column 
-    # vector
-    interpolationAscisseVector3 = linspace(-1, 1, 5)';
-    interpolatedFunctionValuesVector3 = exercise_Internal(...
-        interpolationAscisseVector3, toInterpolateAscisseVector);
-
-    # initialing the real function value
-    #errorVector = toInterpolateAscisseVector;
-    #for i=1:length(toInterpolateAscisseVector)
-    #    errorVector = realFunction(toInterpolateAscisseVector(i)) ...
-    #        - toInterpolateOrdinateVector(i);
-    #end
-
-    #scale = 1:1:length(interpolationAscisseVector);
-    #semilogy(scale, errorVector, "b");
-    #grid;
-    #print 'exercise47-ErrorPlotOutput.tex' '-dTex' '-S800, 600';    
-
-    plot(toInterpolateAscisseVector, interpolationOrdinateVector, "b", ...
-        toInterpolateAscisseVector, interpolatedFunctionValuesVector1, "c", ...
-         toInterpolateAscisseVector, interpolatedFunctionValuesVector2, "g", ...
-           toInterpolateAscisseVector, interpolatedFunctionValuesVector3, "r");
+    # plot the errors
+    domain = [1:1:errorVectorDimension]';
+    semilogy(domain, rungeErrorVector, "b", ...
+        domain, bernsteinErrorVector, "r");
     grid;
-    print 'exercise411-CorrectPlotOutput.tex' '-dTex' '-S800, 600';
+    print 'exercise411-ErrorsSemilogYPlotOutput.tex' '-dTex' '-S800, 600';
+
+    loglog(domain, rungeErrorVector, "b", ...
+        domain, bernsteinErrorVector, "r");
+    grid;
+    print 'exercise411-ErrorsLoglogPlotOutput.tex' '-dTex' '-S800, 600';
 
 endfunction
 
-function [interpolatedFunctionValuesVector] = exercise_Internal(...
-    interpolationAscisseVector, toInterpolateAscisseVector)
-
-    interpolationOrdinateVector = interpolationAscisseVector;
-    for i=1:length(interpolationAscisseVector)
-        interpolationOrdinateVector(i) = realFunction(interpolationAscisseVector(i));
-    end
-
-    differenzeDiviseVector = differenzeDiviseEngine(interpolationAscisseVector, ...
-        interpolationOrdinateVector);
-
-    interpolatedFunctionValuesVector = HornerGeneralizzato(toInterpolateAscisseVector, ...
-        interpolationAscisseVector, differenzeDiviseVector);
+function [y] = rungeRealFunction(x)
+    y = 1/(1 + x^(2));
 endfunction
 
-function [y] = realFunction(x)
+function [y] = bernsteinRealFunction(x)
     y = abs(x);
 endfunction
