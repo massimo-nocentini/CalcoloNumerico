1cf12c3728ad50c71ee19ac0b8a4ce40b85b903c
Massimo Nocentini <massimo.nocentini@gmail.com>
2011-06-02 07:12:10 +0000

    fixed exercises 4.11 and 4.15
    git-svn-id: https://mnserver.homeunix.net/svn/MyRepository/Study/LaureaInformatica/SecondoAnno/Calcolo Numerico/Elaborato@764 a5b5ea6b-43ee-a641-ac96-51f52e225603

--- listings/chapterFour/exercises411415CommonFactor.m
+++ listings/chapterFour/exercises411415CommonFactor.m
@@ -1,9 +1,10 @@
-function [domain, rungeErrorVector, bernsteinErrorVector] = ...
-    exercises411415CommonFactor(...
-      errorIntervalVector, ascisseCreationFunctionName)
+function [domain, rungeErrorVector, bernsteinErrorVector, ...
+    rungeEvaluationIntervalOutput, rungeInterpolatedOrdinateVectorOutput,...
+    bernsteinEvaluationIntervalOutput, bernsteinInterpolatedOrdinateVectorOutput] = ...
+      exercises411415CommonFactor(errorIntervalVector, ascisseCreationFunctionName)
 
     # build the vectors for valuate the interpolation and study the error.
-    evaluateSetDimension = 100;
+    evaluateSetDimension = 10^(3);
     rungeEvaluationInterval = linspace(-5, 5, evaluateSetDimension)';
     bernsteinEvaluationInterval = linspace(-1, 1, evaluateSetDimension)';
 
@@ -12,73 +13,79 @@ function [domain, rungeErrorVector, bernsteinErrorVector] = ...
     bernsteinEvaluatedFunctionValuesVector = bernsteinEvaluationInterval;
     
      # build the real functions values for evaluate real functions
-    for i=1:evaluateSetDimension
-        rungeEvaluatedFunctionValuesVector(i) = rungeRealFunction(...
-            rungeEvaluationInterval(i));
+    rungeEvaluatedFunctionValuesVector = rungeRealFunction(...
+      rungeEvaluationInterval);
+    bernsteinEvaluatedFunctionValuesVector = bernsteinRealFunction(...
+      bernsteinEvaluationInterval);
 
-        bernsteinEvaluatedFunctionValuesVector(i) = bernsteinRealFunction(...
-            bernsteinEvaluationInterval(i));
-    end 
-
-    # build the error vector
-    errorVectorDimension = evaluateSetDimension;#max(errorIntervalVector);
+    # get the error vector dimension
+    errorVectorDimension = length(errorIntervalVector);
+    
+    # initializing the vectors with a dummy value
     rungeErrorVector = ones(errorVectorDimension,1);
     bernsteinErrorVector = ones(errorVectorDimension,1);
 
     # start from 1 to have at least one ascisse for Bernstein interval domain.
-    for n=errorIntervalVector
+    for i=1:errorVectorDimension
 
+        # current number of ascisse to build the approximation
+        # +1 because n is the rank, n+1 are the ascisse
+        n = errorIntervalVector(i) + 1;
+        
         # build the ascisse where I want to interpolate the functions
         # the limits {-5, 5, -1, 1} are fixed by definition of the exercise
         # fix the length of the set of interpolation ascisse
-        #ascisseDimension = 10;
+        # using the strategy that is passed as parameter
         rungeInterpolationAscisseVector = feval(ascisseCreationFunctionName, ...
             -5, 5, n)';
-
         bernsteinInterpolationAscisseVector = feval(ascisseCreationFunctionName, ...
             -1, 1, n)';
 
-        # setup the arrays of function values with the correct dimensions
-        rungeInterpolationOrdinateVector = rungeInterpolationAscisseVector;
-        bernsteinInterpolationOrdinateVector = bernsteinInterpolationAscisseVector;
-
-        # build the real functions values for building the interpolation polynomion
-        for i=1:n
-            rungeInterpolationOrdinateVector(i) = rungeRealFunction(...
-                rungeInterpolationAscisseVector(i));
-
-            bernsteinInterpolationOrdinateVector(i) = bernsteinRealFunction(...
-                bernsteinInterpolationAscisseVector(i));
-        end    
+        # compute the function values on each interpolation ascisse       
+        rungeInterpolationOrdinateVector = rungeRealFunction(...
+            rungeInterpolationAscisseVector);
+        bernsteinInterpolationOrdinateVector = bernsteinRealFunction(...
+            bernsteinInterpolationAscisseVector);
 
         # compute the differenze divise vectors for the two functions.
-        rungeDifferenzeDiviseVector = differenzeDiviseEngine(rungeInterpolationAscisseVector, ...
-            rungeInterpolationOrdinateVector);
-
+        rungeDifferenzeDiviseVector = differenzeDiviseEngine(...
+          rungeInterpolationAscisseVector, rungeInterpolationOrdinateVector);
         bernsteinDifferenzeDiviseVector = differenzeDiviseEngine(...
             bernsteinInterpolationAscisseVector, bernsteinInterpolationOrdinateVector);
 
         # compute the polynomion and valuate on an interval
-        rungeInterpolatedOrdinateVector = HornerGeneralizzato(rungeEvaluationInterval, ...
+        rungeInterpolatedOrdinateVector = HornerGeneralizzato(...
+            rungeEvaluationInterval, ...
             rungeInterpolationAscisseVector, rungeDifferenzeDiviseVector);
-
-        bernsteinInterpolatedOrdinateVector = HornerGeneralizzato(bernsteinEvaluationInterval, ...
+        bernsteinInterpolatedOrdinateVector = HornerGeneralizzato(...
+            bernsteinEvaluationInterval, ...
             bernsteinInterpolationAscisseVector, bernsteinDifferenzeDiviseVector);
 
+        if i == errorVectorDimension
+          # saving the value for output and outside plot the approximated functions
+          rungeEvaluationIntervalOutput = rungeEvaluationInterval;
+          rungeInterpolatedOrdinateVectorOutput = rungeInterpolatedOrdinateVector;
+          
+          bernsteinEvaluationIntervalOutput = bernsteinEvaluationInterval;
+          bernsteinInterpolatedOrdinateVectorOutput = ...
+            bernsteinInterpolatedOrdinateVector;
+        end
+
         # calculate the error
-        rungeErrorVector(n) = max(abs(rungeEvaluatedFunctionValuesVector ...
-             - rungeInterpolatedOrdinateVector));
-        bernsteinErrorVector(n) = max(abs(bernsteinEvaluatedFunctionValuesVector ...
+        rungeErrorVector(i) = max(abs(rungeEvaluatedFunctionValuesVector ...
+             - rungeInterpolatedOrdinateVector));             
+        bernsteinErrorVector(i) = max(abs(bernsteinEvaluatedFunctionValuesVector ...
              - bernsteinInterpolatedOrdinateVector));
 
     end
        
+    # costruisco il dominio su cui rappresentare gli errori commessi
     domain = [1:1:errorVectorDimension];
     
 endfunction
 
 function [y] = rungeRealFunction(x)
-    y = 1/(1 + x^(2));
+    y = 1./(1 .+ x.^(2));
 endfunction
 
 function [y] = bernsteinRealFunction(x)
